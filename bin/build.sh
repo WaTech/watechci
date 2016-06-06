4722f4f16a46a211bb049d59383a67395833ebf8
alexiasa <alexiasa@users.noreply.github.com>
2016-06-06 16:22:54 -0700

    Create build.sh

---
+++ bin/build.sh
@@ -0,0 +1,9 @@
+
+#!/bin/bash
+set -x
+
+# Build the site with Jekyll
+#bundle exec jekyll build
+
+# Compress assets with Zopfli
+#opfli/zopfli --i1000 _site/**/*.html _site/*.html  _site/**/*.css _site/*.css _site/**/*.js _site/*.js _site/**/*.xml _site/*.xml
